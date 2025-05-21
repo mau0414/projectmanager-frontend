9bfdf409b421b8f6593c26ebdd7b90584a0819af
mauricio-gallera <galmeidamauricio12@gmail.com>
2025-05-21 19:02:27 -0300

    fist commit

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,36 @@
+import './style.css'
+import api from '../../services/api'
+import { useRef } from 'react'
+
+function Home() {
+
+  const nameRef = useRef();
+
+  async function exemplo() {
+
+    const a = await api.get("/")
+  }
+
+  async function login() {
+
+    console.log(nameRef);
+    // api.post("/login");
+  }
+
+  return (
+    <>
+      <div className='container'>
+        <form>
+          <h1>Faça login</h1>
+          <input name='name' type='text' ref={nameRef}/>
+          <button type='button' onClick={login}>Entrar</button>
+        </form>
+        <button>
+          
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default Home
---
+++ src/pages/Projects/index.jsx
@@ -0,0 +1,58 @@
+import './style.css'
+import Trash from '../../assets/trash-svgrepo-com.svg'
+import Edit from '../../assets/edit-svgrepo-com.svg'
+import Open from '../../assets/open-in-new-window-button-1-svgrepo-com.svg'
+import { useEffect, useState } from 'react';
+import api from '../../services/api'
+
+// apenas useState pode mudar coisas da tela
+// useState eh um hook, que eh uma ferramenta de suporte
+// pegar .data da resposta da api
+
+function Home() {
+
+    const a = 1;
+
+    const [projects, setProjects] = useState([])
+
+    useEffect(() => {
+
+        setProjects([{
+            name: "mauricio",
+            startDate: "a",
+            endDate: "b"
+        }])
+
+    }, [])
+
+    return (
+
+
+        <>
+            <div className='container'>
+
+                {projects.map(project => (
+                    <div key={project.id}>
+                        <div>
+                            <p>Nome: {project.name}</p>
+                            <p>Data de início: {project.startDate}</p>
+                            <p>Data de fim: {project.endDate}</p>
+                        </div>
+                        <button>
+                            <img src={a} />
+                        </button>
+                        <button>
+                            <img src={a} />
+                        </button>
+                        <button>
+                            <img src={a} />
+                        </button>
+                    </div>
+                ))}
+
+            </div>
+        </>
+    )
+}
+
+export default Home
